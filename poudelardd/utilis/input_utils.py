baffe83e976d32eb58383c85f23b71b342afa752
warren <warrenchen,dm@gmail,com>
2025-12-01 10:44:29 +0100

    3.1-3.1.3 update 12/01/2025

--- poudelardd/utilis/input_utils.py
+++ poudelardd/utilis/input_utils.py
@@ -1,3 +1,11 @@
+#1
+def demander_texte(messages):
+    valide= False
+    while valide == False:
+        dmd = input(messages)
+        if dmd.strip() !="":
+            valide = True
+#2
 def demander_nombre(message,val_min,val_max):
     valide=true
     while valide==true:
@@ -5,10 +13,17 @@ def demander_nombre(message,val_min,val_max):
         if n>val_max or n<val_min:
             valide=false
             print("Veuillez entrer un nombre entre",val_min,"et",val_max)
-
-
-
-
+    return
+#3
+def demander_choix(messages,options):
+    olala = False
+    while olala == False:
+        print(messages)
+        for i in range(len(options)):
+            print(i+1.".",options[i])
+        if demander_nombre("Votre choix :",0,len(options)):
+            olala= True
+        return choix
 
 
 
