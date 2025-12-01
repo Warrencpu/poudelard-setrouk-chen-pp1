c5040f8165a5f10421edde0e107dfc8bfde72479
warren <warrenchen,dm@gmail,com>
2025-12-01 11:10:22 +0100

    3.1-3.1.4 update 12/01/2025

--- poudelardd/utilis/input_utils.py
+++ poudelardd/utilis/input_utils.py
@@ -6,26 +6,35 @@ def demander_texte(messages):
         if dmd.strip() !="":
             valide = True
 #2
-def demander_nombre(message,val_min,val_max):
+def demander_nombre(message,val_min=None,val_max=None):
     valide=true
-    while valide==true:
-        n = input("Niveau de courage (1-10) : ", val_min, val_max)
-        if n>val_max or n<val_min:
-            valide=false
-            print("Veuillez entrer un nombre entre",val_min,"et",val_max)
-    return
+    while valide==True:
+        n = input(("Niveau de courage (1-10) : "),message)
+        if n=> val_min and n =< val_max:
+            print("Niveau de courage (1-10) : ", n)
+            valide = True
+        else
+            print("Veuillez saisir une valeur valide")
+            valide= False
+        return n
 #3
 def demander_choix(messages,options):
     olala = False
     while olala == False:
         print(messages)
         for i in range(len(options)):
-            print(i+1.".",options[i])
+            print(i+1,".",options[i])
         if demander_nombre("Votre choix :",0,len(options)):
             olala= True
         return choix
 
+#4
+import json
 
+def load_fichier(chemin_fichier):
+    with open (chemin_fichier,"r",encoding='utf-8') as f:
+        dico = json.load(f)
+    return dico
 
 
 
