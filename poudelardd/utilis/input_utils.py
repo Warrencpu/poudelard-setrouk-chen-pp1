42d436d3ec2bfa45a0fa8fe40aeb308dbc62e68e
warren <warrenchen,dm@gmail,com>
2025-12-12 13:10:34 +0100

    sauvegarde Elsa patch 1:10

--- poudelardd/utilis/input_utils.py
+++ poudelardd/utilis/input_utils.py
@@ -1,39 +1,42 @@
-import json
 #1
 def demander_texte(messages):
-    valide = False
+    valide= False
     while valide == False:
         dmd = input(messages)
-        if dmd.strip() != "" :
+        if dmd.strip() !="":
             valide = True
-    return dmd.strip()
-#2
+    return dmd
+
+def nb_existe(nb):
+    contient_lettre = False
+    for carac in nb:
+        if carac not in  '0123456789':
+            contient_lettre = True
+    return contient_lettre
+
 def demander_nombre(message,val_min=None,val_max=None):
-    valide = true
-    while valide == True:
-        m = demander_texte(input(message))
-        for i in m :
-            if i is int or i == "-" :
-                if m >= val_min and m <= val_max:
-                    print(message , n)
-                    valide = False
-    return m
-#3
+    nb = 'a'
+    saisie = False
+    while saisie == False :
+        nb = input(message)
+        if nb_existe(nb) == False or nb[0] == '-' and len(nb) > 1 and nb_existe(nb[1:])== False:
+            nb = int(nb)
+            if val_min is not None or val_max is not None   :
+                if nb >= val_min or nb <= val_max:
+                    saisie = True
+
+
 def demander_choix(messages,options):
-    correct = False
-    while correct == False:
-        print(messages)
-        for i in range(len(options)):
-            print(i + 1 ,".", options[i])
-        if demander_nombre("Votre choix :",0,len(options)):
-            correct = True
-        return choix
+    print(messages)
+    for i in range(len(options)):
+        print(i+1,".",options[i])
+    choix = demander_nombre('votre choix' , 1,i-1)
+    return choix
 
 #4
+import json
 
 def load_fichier(chemin_fichier):
     with open (chemin_fichier,"r",encoding='utf-8') as f:
         dico = json.load(f)
     return dico
-
-
