fb0d7fae89a2d6c6f6b9d4e6ac94bab96f0b2018
elsas <elsa.setrouk@gmail.com>
2025-12-12 11:18:05 +0100

    demander nombre

--- poudelardd/utilis/input_utils.py
+++ poudelardd/utilis/input_utils.py
@@ -1,35 +1,45 @@
-import json
 #1
 def demander_texte(messages):
-    valide = False
+    valide= False
     while valide == False:
         dmd = input(messages)
-        if dmd.strip() != "" :
+        if dmd.strip() !="":
             valide = True
-    return dmd.strip()
-#2
+    return dmd
+
+
 def demander_nombre(message,val_min=None,val_max=None):
-    valide = true
-    while valide == True:
-        m = demander_texte(input(message))
-        for i in m :
-            if i is int or i == "-" :
-                if m >= val_min and m <= val_max:
-                    print(message , n)
-                    valide = False
-    return m
-#3
+    valide = False
+    while valide == False :
+        n = input(message)
+        v = True
+        for i in n :
+            i = ord(i)
+            if ord('a')< i <ord('z') or ord('A') < i < ord('Z'):
+                v = False
+            elif val_max is not None and val_min is not None:
+                if  ord(n) < val_min or  ord(n)> val_max :
+                    v = False
+        if v == True :
+            valide = True
+        return ord(n)
+
+
+
+
+
 def demander_choix(messages,options):
-    correct = False
-    while correct == False:
+    valide = False
+    while valide == False:
         print(messages)
         for i in range(len(options)):
-            print(i + 1 ,".", options[i])
+            print(i+1,".",options[i])
         if demander_nombre("Votre choix :",0,len(options)):
-            correct = True
-        return choix
+            valide = True
+        return valide
 
 #4
+import json
 
 def load_fichier(chemin_fichier):
     with open (chemin_fichier,"r",encoding='utf-8') as f:
@@ -37,13 +47,4 @@ def load_fichier(chemin_fichier):
     return dico
 
 
-#5
-def recevoir_lettre():
-    lettre=int(input("Une chouette traverse la fenêtre et vous apporte une lettre scellée du sceau de Poudlard... « Cher élève, Nous avons le plaisir de vous informer que vous avez été admis à l’école de sorcellerie de Poudlard ! » Souhaitez-vous accepter cette invitation et partir pour Poudlard ? 1. Oui, bien sûr ! 2. Non, je préfère rester avec l’oncle Vernon... "))
-    if lettre == 1:
-       print("Votre choix:", lettre)
-    else:
-        print("Vous déchirez la lettre, l’oncle Vernon pousse un cri de joie: « EXCELLENT ! Enfin quelqu’un de NORMAL dans cette maison ! » Le monde magique ne saura jamais que vous existiez... Fin du jeu. ")
-        exit(0)
-
-
+demander_nombre("caca",0,10)
