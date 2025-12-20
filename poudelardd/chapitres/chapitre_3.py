d60fca7ce236aac35a19f5c72e4942382f351a25
warren <warrenchen,dm@gmail,com>
2025-12-20 15:51:33 +0100

    apprendre sort mais pas opti

--- poudelardd/chapitres/chapitre_3.py
+++ poudelardd/chapitres/chapitre_3.py
@@ -1,16 +1,47 @@
 from poudelardd.univers.maison import repartition_maisons
 from poudelardd.univers.personnage import*
+import json
 import random
 
-def apprendre_sorts(joueur,load_fichier='../data/sorts.json'):
-    print("Tu commences tes cours de magie a Poudlard")
-    print("Tu viens d'apprendre le sortilege :")
-    if random.choice = "Offensif":
-        joueur['Sortilege']['Offensif'] += 1
-    if random.choice = "Defensif":
-        joueur['Sortilege']['Defensif'] += 1
-    print("Bravo vous avez appris un nouveau sortilege",)
-    print("Appuyez pour continuer")
+def apprendre_sorts(joueur, chemin_fichier="../data/sorts.json"):
+    fichier = open(chemin_fichier, "r", encoding="utf-8")
+    sorts = json.load(fichier)
+    fichier.close()
+    offensif = 0
+    defensif = 0
+    utilitaire = 0
+    while len(joueur["sortileges"]) != 5:
+        i = random.randint(0, len(sorts) - 1)
+        sort = sorts[i]
+        deja_pris = False
+        for s in joueur["sortileges"]:
+            if s == sort:
+                deja_pris = True
+        if deja_pris == True:
+            continue
+        if sort["type"] == "offensif":
+            if offensif < 1:
+                joueur["sortileges"].append(sort)
+                offensif = offensif + 1
+                print("Sort appris :", sort["nom"], "(" + sort["type"] + ")")
+        if sort["type"] == "defensif":
+            if defensif < 1:
+                joueur["sortileges"].append(sort)
+                defensif = defensif + 1
+                print("Sort appris :", sort["nom"], "(" + sort["type"] + ")")
+        if sort["type"] == "utilitaire":
+            if utilitaire < 3:
+                joueur["sortileges"].append(sort)
+                utilitaire = utilitaire + 1
+                print("Sort appris :", sort["nom"], "(" + sort["type"] + ")")
+    print("")
+    print("Recapitulatif :")
+    for sort in joueur["sortileges"]:
+        print(sort["nom"])
+        print("type :", sort["type"])
+        print("description :", sort["description"])
+        print("")
+
 
 def quiz_magie(joueur, chemin_fichier="../data/quiz_magie.json"):
     questions = load_fichier(chemin_fichier)
