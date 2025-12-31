cc75c6464bdc156231dda501a5c0df3164dc42ae
elsas <elsa.setrouk@gmail.com>
2025-12-31 13:08:27 +0100

    Début chapitre 4

---
+++ poudelardd/chapitres/chapitre_4.py
@@ -0,0 +1,12 @@
+
+def initialiser_voldemor():
+    voldemor ={"Nom": "Lord",
+                 "Prenom": "Voldemor",
+                "Argent":100 ,
+                 "Inventaire": [],
+                 "Sortilèges": [],
+                "Attributs": {"Courage": 0,
+                                "Intelligence": 0,
+                                 "Loyauté" : 0,
+                                 "Ambition": 1000000}}
+    # mettre sortileges par rapport a la difficulté choisit
