b3110cc53fe9d1496baec333530504355d89b466
elsas <elsa.setrouk@gmail.com>
2026-01-03 18:34:21 +0100

    Fin chapitre 4

--- poudelardd/chapitres/chapitre_4.py
+++ poudelardd/chapitres/chapitre_4.py
@@ -40,26 +40,51 @@ def initialiser_voldemor():
         "description": "Réanime une personne assommée ou inconsciente.",
         "type": "Défensif"
         } ]
+        if diff == 3:
+            voldemor["PV"] = 5
 
     return voldemor
 
+def Attaques(joueur1,attaque,sort_ennemi):
+    Sort_caracteristiques = {"Utilitaire": [["Utilitaire", 0], ["Offensif", 1], ["Défensif", -1]],
+                             "Offensif": [["Utilitaire", -1], ["Offensif", 0], ["Défensif", 1]],
+                             "Défensif": [["Utilitaire", 1], ["Offensif", -1], ["Défensif", 0]]}
+    cpt = 0
+    for i in Sort_caracteristiques[joueur1["Sortilèges"][attaque-1]["type"]]:
+        if i[0] == sort_ennemi["type"]:
+            puissance = Sort_caracteristiques[joueur1["Sortilèges"][attaque - 1]["type"]][cpt][1]
+        cpt = cpt+ 1
+    return puissance
+
+
+def probabilités(joueur1, joueur2):
+    for i in range (len(joueur1["Sortilèges"])):
+        cpt_u = cpt_d = cpt_o = 0
+        for j in joueur2["Sortilèges"]:
+            if j["type"] == "Utilitaire":
+                cpt_u = cpt_u + 1
+            elif j["type"] == "Défensif":
+                cpt_d = cpt_d + 1
+            elif j["type"] == "Offensif":
+                cpt_o = cpt_o + 1
+        cp_total = cpt_u + cpt_d +cpt_o
+        if joueur1["Sortilèges"][i]["type"] == "Utilitaire":
+            joueur1["Sortilèges"][i]["Probabilités"] = cpt_o *100 // cp_total
+        elif joueur1["Sortilèges"][i]["type"]== "Défensif":
+            joueur1["Sortilèges"][i]["Probabilités"] = cpt_u*100 // cp_total
+        if joueur1["Sortilèges"][i]["type"]== "Offensif":
+            joueur1["Sortilèges"][i]["Probabilités"] = cpt_d*100 //cp_total
+
 
 def manche(joueur1, joueur2):
-    Sort_caracteristiques = {"Utilitaire": [ 0, 1, -1],
-                             "Offensif": [ -1, 0, 1],
-                             "Défensif": [ 1, -1, 0]}
+    probabilités(joueur1,joueur2)
     attaque = demander_choix("quel sort souhaitez vous lancer",joueur1["Sortilèges"])
     input("Vous avez choisi {} ({})".format(joueur1["Sortilèges"][attaque-1]["nom"],joueur1["Sortilèges"][attaque-1]["type"]))
     sort_ennemi = random.choice(joueur2["Sortilèges"])
     input("Voldemort a utilisé {} ({})".format(sort_ennemi["nom"],sort_ennemi["type"]))
-    if sort_ennemi["type"]== "Utilitaire":
-        puissance = Sort_caracteristiques[joueur1["Sortilèges"][attaque-1]["type"]][0]
-    elif sort_ennemi["type"]== "Offensif":
-        puissance = Sort_caracteristiques[joueur1["Sortilèges"][attaque-1]["type"]][1]
-    elif sort_ennemi["type"]== "Défensif":
-        puissance = Sort_caracteristiques[joueur1["Sortilèges"][attaque-1]["type"]][2]
-    return puissance, attaque, sort_ennemi
+    puissance = Attaques(joueur1,attaque,sort_ennemi)
 
+    return puissance, attaque, sort_ennemi
 
 
 def lancer_partie(joueur1,joueur2):
@@ -67,6 +92,7 @@ def lancer_partie(joueur1,joueur2):
     input("Lord Voldemort ! :-O")
     input("Vous pouvez le battre d'une seule facon , en lui donnant 3 coups fatales a l'aide de vos sorts !")
     pv_v = pv_j = 2
+    pv_v = joueur2["PV"]
     fin = False
     while (pv_v > 0 and pv_j !=0) or (pv_v != 0 and pv_j > 0):
         puissance, attaque , sort_ennemi = manche(joueur, voldemort)
